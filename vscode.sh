16ff8dabd82e9150fb13c9454542456d983db8bc
rrkroms <rrkroms.xda@gmail.com>
2023-07-15 01:25:02 +0530

    Add(feature): Introduce ROM_UI (message function) 'Bash plugin' for enhanced message display

--- vscode.sh
+++ vscode.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+source rom_ui
 DISTRO_DIR="${PREFIX}/var/lib/proot-distro/installed-rootfs/ubuntu"
 
 start (){
 if [[ -e ${PREFIX}/bin/proot-distro && -e ${DISTRO_DIR}/usr/bin/code-server ]] ; then
-	echo  "launching code-server by ubuntu proot!"
+	tell i  "launching code-server by ubuntu proot!"
 	proot-distro login ubuntu -- code-server $2 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
-	echo "code-server successfully launched."
+	tell s "code-server successfully launched."
 else
 	[ ! -e $PREFIX/bin/proot-distro ] &&
-	echo "proot-distro not exitestt"
+	tell f "proot-distro not exitestt"
 
 	[ ! -e ${DISTRO_DIR} ] &&
-	echo " ubuntu distro not found"
+	tell f " ubuntu distro not found"
 
 	[ ! -e ${DISTRO_DIR}/usr/bin/code-server ] &&
-	echo "code-server not found, maybe code-server not install in ubuntu distro?"
+	tell f "code-server not found, maybe code-server not install in ubuntu distro?"
 
 fi
 }
 
 stop (){
 task_list="$(pgrep -x node 2>/dev/null)"
-[ -z "$task_list" ] && echo "code-server not running in background" 
+[ -z "$task_list" ] && tell f "code-server not running in background" 
 pkill -x node
 }
 
