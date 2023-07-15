f6f4e7eea3cd469114fa2195ad61ba40aecbdc7f
rrkroms <rrkroms.xda@gmail.com>
2023-07-16 03:53:10 +0530

    Add(Feature | Info): support for Kali Linux(ARM64) distro & installation guide for distros

--- README.md
+++ README.md
@@ -1,12 +1,48 @@
 >**DISCLAIMER:** THIS IS AN ALPHA/EXPERIMENTAL VERSION.
 ## support/Requirements
-- ubuntu distro(`proot-distro`)
+- ubuntu distro or kali-linux (`proot-distro`)
 - proot-distro
 - termux
-## setup launcher
+
+## Installtation
+
+### Setup distro
+- install proot-distro package : `pkg update && pkg install proot-distro`
+> if you are kali linux user, then skip proot-distro installation.
+
+**Setup unbuntu** 
+- install ubuntu distro : `proot-distro install ubuntu`
+
+**Setup kali-linux**
+- install kali linux: https://www.kali.org/docs/nethunter/nethunter-rootless
+
+### Setup code-server
+- to install code-server for ubuntu, execute this command in `termux`  : 
+
+```
+proot-distro login ubuntu -- apt update && proot-distro login ubuntu -- apt install code-server
+```
+
+- kali linux's user run this command in kali linux terminal :
+```
+apt update && apt install code-server
+```
+
+### Setup launcher
 - `install` code-server browser [apk](app/code-server_1.apk).
 - run `chmod -x ./code.sh` to make executable.
 
-## usage
-- run `./vscode.sh -s` to start code-server with code-server web viewer app
-- run `./vscode.sh -q` to stop/kill code-server
+## Usage
+
+| options | usage | work/meaning |
+| ------ | ------ | ------ |
+| -s | ./vscode.sh -s[distro-option] or -[distro-option]s | start code-server on selected proot distro |
+| -q | ./vscode.sh -q | stop/kill code-server |
+| -k | ./vscode.sh -ks or -sk | set `kali linux` distro for code-sever |
+| -u | ./vscode.sh -us or -su | set `ubuntu` distro for code-server |
+
+- to run code-server simply, use distro-option `-u` or `-k` with `-s` start-option.
+example: for ubuntu, usage `./vscode.sh -us ` or `./vscode.sh -su`
+         for kali linux usage `vscode.sh` -uk` or `./vscode,sh` -ku`
+
+- to stop code-server simply, usage `./vscode.sh -q`.
--- vscode.sh
+++ vscode.sh
@@ -1,41 +1,59 @@
 #!/bin/bash
 source rom_ui
-DISTRO_DIR="${PREFIX}/var/lib/proot-distro/installed-rootfs/ubuntu"
 
-start (){
-if [[ -e ${PREFIX}/bin/proot-distro && -e ${DISTRO_DIR}/usr/bin/code-server ]] ; then
-	tell i  "launching code-server by ubuntu proot!"
-	proot-distro login ubuntu -- code-server $2 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
-	tell s "code-server successfully launched."
-else
-	[ ! -e $PREFIX/bin/proot-distro ] &&
-	tell f "ERROR: The 'proot-distro' package is not installed. To run Ubuntu distro, please install it." && 
-	tell d "USAGE: 'pkg install proot-ditro' to install proot-distro"
-	echo
-	[ ! -e ${DISTRO_DIR} ] &&
-	tell f "ERROR: 'ubuntu' distro is not installed. To run code-server, please install it." &&
-	tell d "USAGE: 'proot-distro install ubuntu' to install Ubuntu distro"
-	echo
-	[ ! -e ${DISTRO_DIR}/usr/bin/code-server ] &&
-	tell f "ERROR: 'code-server' package is not installed on ubuntu distro." &&
-	tell d "USAGE: proot-distro login ubuntu -- apt update && proot-distro login ubuntu -- apt install code-server \n     to install code-server in Ubuntu distro."
+if [[ "$1" == "-"*"k"* ]]; then
+	DISTRO_NAME="kali linux"
+	DISTRO_DIR=/data/data/com.termux/files//home/kali-arm64
+	DISTRO_LAUNCHER="nh"
+	DISTRO_LAUNCHER_COM=nh
+	SOLVE_DISTRO="Follow: README.md file to install kali linux with setup for vscode launcher."
+	SOLVE_CODE_SERVER="Usage: 'nh apt update && nh apt install code-server' to install code-srever on ${DISTRO_NAME} distro."
+elif [[ "$1" == "-"*"u"* ]]; then
+	DISTRO_NAME=ubuntu
+	DISTRO_DIR=${PREFIX}/var/lib/proot-distro/installed-rootfs/ubuntu
+	DISTRO_LAUNCHER=proot-distro
+	DISTRO_LAUNCHER_COM="proot-distro login ${DISTRO_NAME} --"
+	SOLVE_DISTRO='Usage: `proot-distro install ubuntu` to install ${DISTRO_NAME}'
+	SOLVE_CODE_SERVER="Usage: `proot-distro login ubuntu -- apt update && proot-distro login ubuntu -- apt install code-server` \n     to install code-server in Ubuntu distro."
 fi
+
+start() {
+	if [[ -e ${PREFIX}/bin/${DISTRO_LAUNCHER} && -e ${DISTRO_DIR}/usr/bin/code-server ]]; then
+		tell i "launching code-server by ${DISTRO_NAME} proot!"
+		${DISTRO_LAUNCHER_COM} code-server $1 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
+		tell s "code-server successfully launched."
+	else
+		[[ "${DISTRO_LAUNCHER}" == "proot-distro" && ! -e ${PREFIX}/bin/proot-distro ]] &&
+			tell f "Error: The 'proot-distro' package is not installed. To run ${DISTRO_NAME} distro, please install it." &&
+			tell d "Usage: 'pkg install proot-ditro' to install proot-distro \n"
+		
+		[ ! -e ${DISTRO_DIR} ] &&
+			tell f "Error: '${DISTRO_NAME}' distro is not installed. To run code-server, please install it." &&
+			tell d "${SOLVE_DISTRO} \n"
+		
+		[ ! -e ${DISTRO_DIR}/usr/bin/code-server ] &&
+			tell f "Error: 'code-server' package is not installed on ${DISTRO_NAME} distro." &&
+			tell d "${SOLVE_CODE_SERVER} \n"
+	fi
 }
 
-stop (){
-task_list="$(pgrep -x node 2>/dev/null)"
-[ -z "$task_list" ] && tell f "code-server is not running in background" 
-pkill -x node
+stop() {
+	task_list="$(pgrep -x node 2>/dev/null)"
+	[ -z "$task_list" ] && tell f "code-server is not running in background"
+	pkill -x node
 }
 
 case $1 in
--q ) stop ;;
--s ) start ;;
-* )
- echo " 	thats programme help to easy to launch vsode server(code-server)
+*q*) stop ;;
+*s*) start ;;
+*)
+	echo " 	thats programme help to easy to launch vscode server(code-server)
 	launcher command:
-	 Ex: [ -s|-u ] [\$3]
-	\$3     argument/option for code-server
+	 Ex: [ -ku|-su|-us|-uk|-s|-q ] [\$3]
+	\$3    argument/option for code-server
 	-s 	start code-server
-	-q	stop code-server" ;;
+	-q	stop code-server
+	-u 	use ubuntu proot
+	-k 	use kali linux proot"
+	;;
 esac
