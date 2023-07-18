55732791ee23baa6a2dcc1d26d3ecf8346b3fbd5
rrkroms <rrkroms.xda@gmail.com>
2023-07-19 03:40:12 +0530

    Cherry-Pick(824b8cc)|Fix(bash argument): issue with code-server not receiving/processing arguments.

--- vscode.sh
+++ vscode.sh
@@ -22,7 +22,8 @@ fi
 start (){
 if [[ -e ${PREFIX}/bin/${CHROOT_LAUNCHER} && -e ${CHROOT_DIR}/usr/bin/code-server ]] ; then
 	tell i "launching code-server by ${CHROOT_NAME} proot!"
-	${CHROOT_LAUNCHER_COM} code-server $2 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
+	${CHROOT_LAUNCHER_COM} code-server $1 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
+
 	tell s "code-server successfully launched."
 else
 	[ ! -e $PREFIX/bin/{$CHROOT_LAUNCHER} ] &&
@@ -42,12 +43,12 @@ pkill -x node
 
 case $1 in
 *q* ) stop ;;
-*s* ) start ;;
+*s* ) start $2 ;;
 * )
  echo " 	thats programme help to easy to launch vsode server(code-server)
 	launcher command:
-	 Ex: [ -ku|-su|-us|-uk|-s|-q ] [\$3]
-	\$3    argument/option for code-server
+	 Ex: [ -s|-u ] [\$2]
+	\$2   argument/option for code-server
 	-s 	start code-server
 	-q	stop code-server
 	-u 	use ubuntu proot
