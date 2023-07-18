042faf1329a289a62b20caeaeec9af60ee769237
rrkroms <rrkroms.xda@gmail.com>
2023-07-19 03:53:10 +0530

    add(feature): kali linux distro support

--- vscode.sh
+++ vscode.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 source rom_ui
+
+if [[ "$1" == "-"*"k"* ]] ; then
+	CHROOT_CODE_NAME=kali
+	CHROOT_USER_NAME=kali
+	CHROOT_NAME="kali linux"
+	CHROOT_DIR=/data/data/com.termux/files//home/kali-arm64
+	CHROOT_LAUNCHER="nh"
+	CHROOT_LAUNCHER_COM=nh
+else
+   if	[[ "$1" == "-"*"u"* ]] ; then
+	CHROOT_CODE_NAME=ubuntu
+        CHROOT_USER_NAME=ubuntu
+        CHROOT_NAME=ubuntu
+        CHROOT_DIR=/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu
+        CHROOT_LAUNCHER=proot-distro
+	CHROOT_LAUNCHER_COM="proot-distro login ${CHROOT_NAME} --"
+   fi
+fi
+
 start (){
-if [[ -e ${PREFIX}/bin/proot-distro && -e /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/usr/bin/code-server ]] ; then
-	tell i "launching code-server by ubuntu proot!"
-	proot-distro login ubuntu -- code-server $2 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
+if [[ -e ${PREFIX}/bin/${CHROOT_LAUNCHER} && -e ${CHROOT_DIR}/usr/bin/code-server ]] ; then
+	tell i "launching code-server by ${CHROOT_NAME} proot!"
+	${CHROOT_LAUNCHER_COM} code-server $2 & sleep 7 ; am start --user 0 -n org.chromium.webapk.a18f37c7c4dc2dd10_v2/org.chromium.webapk.shell_apk.h2o.H2OTransparentLauncherActivity
 	tell s "code-server successfully launched."
 else
-	[ ! -e $PREFIX/bin/proot-distro ] &&
+	[ ! -e $PREFIX/bin/{$CHROOT_LAUNCHER} ] &&
 	tell f " proot ${CHROOT_NAME} not found"
 
 	[ ! -e '${CHROOT_DIR}/bin/code-server' ] &&
-	tell f "code-server not found in ubuntu proot, maybe code-server not install in proot?"
+	tell f "code-server not found in ${CHROOT_NAME} proot, maybe code-server not install in proot?"
 
 fi
 }
@@ -22,13 +41,15 @@ pkill -x node
 }
 
 case $1 in
--q ) stop ;;
--s ) start ;;
+*q* ) stop ;;
+*s* ) start ;;
 * )
  echo " 	thats programme help to easy to launch vsode server(code-server)
 	launcher command:
-	 Ex: [ -s|-u ] [\$3]
+	 Ex: [ -ku|-su|-us|-uk|-s|-q ] [\$3]
 	\$3    argument/option for code-server
 	-s 	start code-server
-	-q	stop code-server" ;;
+	-q	stop code-server
+	-u 	use ubuntu proot
+	-k 	use kali linux proot" ;;
 esac
